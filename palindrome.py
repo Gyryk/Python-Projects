92ea673128754b0fcc3cd6bf78b206bc5cb7ca47
Girik <68961075+Gyryk@users.noreply.github.com>
2024-09-27 20:02:27 +0100

    Use regex

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,5 @@
+import re
+
 # Functions to get new strings
 def reverseString(stringToReverse):
     global reversedString;
@@ -7,17 +9,13 @@ def reverseString(stringToReverse):
 
 def parseString(stringToConvert):
     global parsedString;
-    global alphanumerics;
 
     lowerCase = stringToConvert.lower();
     
-    for char in lowerCase:
-        if char in alphanumerics:
-            parsedString += char;
+    parsedString = ''.join(re.findall(r'[a-z0-9]', lowerCase))
 
 
 # Variables to store
-alphanumerics = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
 parsedString = "";
 reversedString = "";
 
@@ -27,17 +25,13 @@ print("Welcome to the palindrome checker \n");
 inputString = input("Enter a word: \n");
 
 parseString(inputString);
-if parsedString != "":
+if len(parsedString) > 0:
     reverseString(parsedString);
-elif parsedString == "":
-    print("\n Invalid Input: Please try again with alphabets or numbers. \n")
 else:
-    print("\n Parse Error: Please try again.")
+    print("\n Invalid Input: Please try again with alphabets or numbers. \n")
 
 # Handle results
 if parsedString == reversedString:
     print("\n The word or sentence you have entered is a palindrome! \n");
-elif parsedString != reversedString:
-    print("\n The word or sentence you have entered is not a palindrome \n");
 else:
-    print("\n Check Error: Please try again. \n")
+    print("\n The word or sentence you have entered is not a palindrome \n");
